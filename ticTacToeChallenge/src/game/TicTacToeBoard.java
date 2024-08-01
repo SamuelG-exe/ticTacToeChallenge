3a05fd9a7c8fc7550541f225626748e31a72fda7
Samuel Galbraith <samuelgalbraith.school@gmail.com>
2024-08-01 12:01:21 -0600

    Constructors + planned out the Classes for board/game

--- ticTacToeChallenge/src/game/TicTacToeBoard.java
+++ ticTacToeChallenge/src/game/TicTacToeBoard.java
@@ -1,4 +1,22 @@
 package game;
 
 public class TicTacToeBoard {
+
+    //could use int or Char but String will give me a bit more freedom as I develop
+    private String[][] gameBoard;
+
+    TicTacToeBoard(){
+        gameBoard = new String[3][3];
+    }
+
+    /*
+    I need a few functions for sure
+    1)Reset - for new games and testing
+    2)getXorO - need to access the board's individual locations
+    3)placeXO - need to be able to set a locations piece to an X or O
+
+
+     */
+
+
 }
--- ticTacToeChallenge/src/game/TicTacToeGame.java
+++ ticTacToeChallenge/src/game/TicTacToeGame.java
@@ -1,4 +1,37 @@
 package game;
 
 public class TicTacToeGame {
+    private Team turn;
+
+
+    public Team winner;
+    public boolean tie;
+    public boolean gameOver;
+    public TicTacToeBoard gameboeard;
+
+    //I am using an enum for my own clarity and for consistent reference of team between classes/packages
+    public enum Team{
+        X,
+        O
+    }
+    TicTacToeGame(boolean botGoesFirst){
+        if(botGoesFirst){
+            //bot take turn
+        }
+        gameboeard = new TicTacToeBoard();
+        gameOver = false;
+
+    }
+        /*
+    1)play - need something to call the placing of the board and check legality of move
+        i)bottaketurn- either needs to be a call to anotheer class or a function but should happen
+            after user's turn
+        ii)changeTurn - might not need if I always have the bot go after the player but having it might
+        still be a good idea if I wanted to implement 2 player or just to ensure legality of moves
+    2)checkGameStatus - I need something to check for game over or tie - might need to add some iter in board
+
+
+     */
+
+
 }
