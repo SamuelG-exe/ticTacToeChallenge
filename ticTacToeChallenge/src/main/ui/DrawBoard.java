3035348c4bdded0475178b5e69aa4b6ace3b38c2
Samuel Galbraith <samuelgalbraith.school@gmail.com>
2024-08-01 14:17:28 -0600

    Wrote DrawBoard

--- ticTacToeChallenge/src/main/ui/DrawBoard.java
+++ ticTacToeChallenge/src/main/ui/DrawBoard.java
@@ -1,4 +1,29 @@
 package ui;
 
+import main.game.TicTacToeBoard;
+import main.game.XOPosition;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class DrawBoard {
+    static final PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+
+    public static void drawboard(TicTacToeBoard board){
+        out.println();
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                XOPosition newLocation = new XOPosition(i, j);
+                if(j == 2 ){                                            //    This Line is meant to ensure that when printing it is in rows of 3.
+                    out.println(board.getPiece(newLocation));
+                }
+                else {
+                    out.print(board.getPiece(newLocation)+" ");
+                }
+
+            }
+        }
+
+    }
 }
