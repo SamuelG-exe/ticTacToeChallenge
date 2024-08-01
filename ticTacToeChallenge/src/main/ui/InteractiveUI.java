efd027518d2f00b99e2089fc1baa90f3d79f0de3
Samuel Galbraith <samuelgalbraith.school@gmail.com>
2024-08-01 14:50:49 -0600

    Finished PreGame, can exit on input

--- ticTacToeChallenge/src/main/ui/InteractiveUI.java
+++ ticTacToeChallenge/src/main/ui/InteractiveUI.java
@@ -15,14 +15,14 @@ public class InteractiveUI {
 
 
     public static void main(String[] args) throws Exception {
-        out.println("----Welcome to TicTacToe----");
+        out.println("X----Welcome to TicTacToe----O");
         status = UserStatus.MENU;
         runStateMachine();
     }
 
     private static void runStateMachine() {
 
-        while (true){
+        while (true) {
             runMenuUI();
             runInGameUI();
         }
@@ -30,27 +30,42 @@ public class InteractiveUI {
 
     }
 
-    private static void runInGameUI() {
-        while (status == UserStatus.INGAME){
-            //prompt
-            //grab input
-            //change UserStatus or exit program
-        }
-    }
-
     private static void runMenuUI() {
-        while(status == UserStatus.MENU){
-            //prompt
-            //grab input
-            //change pregame or exit
-            while(preGame){
+        while (status == UserStatus.MENU) {
+            while (preGame) {
+                out.println();
+                out.println();
+                out.println("Would you like to play a game of tic tac toe or close the program?");
+                out.println("Please enter \"1\" if you would like play a game and \"3\" if you would like to close the program");
+                String input = scanner.nextLine().toLowerCase();
+                switch (input) {
+                    case "1":
+                        preGame = false;
+                        break;
+                    case "3":
+                        System.exit(0);
+                        break;
+                    default:
+                        out.println("Sorry I didnt quite understand that, lets try again!");
+                        break;
+                }
                 //prompt
                 //grab input
-                //change UserStatus or exit
-
+                //change UserStatus or exit program or loop again
             }
+
         }
     }
 
-
+    private static void runInGameUI() {
+        while (status == UserStatus.INGAME) {
+            //prompt
+            //grab input
+            //change UserStatus or exit program or loop again
+        }
+    }
 }
+
+
+
+
